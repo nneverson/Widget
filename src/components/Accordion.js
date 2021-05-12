dbc450acae764fcc87a63a196bc80163fd3c2c39
Neely <neely@Neelys-MBP.HPH.Local>
2021-05-12 15:34:11 -0500

    accordion functional

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ toDisplay }) => {
+	const [activeIndex, setActiveIndex] = useState(null);
+
 	const onTitleClick = (index) => {
-		console.log('Title clicked', index);
+		setActiveIndex(index);
+		console.log('title clicked', index);
 	};
 
 	const renderedItems = toDisplay.map((item, index) => {
+		const active = index === activeIndex ? 'active' : '';
 		return (
 			<React.Fragment key={item.title}>
-				<div className='title active' onClick={() => onTitleClick(index)}>
+				<div className={`title ${active}`} onClick={() => onTitleClick(index)}>
 					<i class='dropdown icon'></i>
 					{item.title}
 				</div>
-				<div className='content active'>
+				<div className={`content ${active}`}>
 					<p>{item.content}</p>
 				</div>
 			</React.Fragment>
@@ -22,3 +26,8 @@ const Accordion = ({ toDisplay }) => {
 	return <div className='ui styled accordion'>{renderedItems}</div>;
 };
 export default Accordion;
+
+//How to add state to the functional component
+//line 1 add useState in the react import -> import React, { useState } from "react"
+//line 5 at the top of the component initialize a new piece of state
+//In the helper method update state. The value of state is now the index of the div that was clicked on by the user and it is being printed in the browser (line 27)
